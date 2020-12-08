c9ef0ac3b2248fbc2e7a9ad79d10c0c0f3bdd8e4
sanggayasa <sanggayasa502@gmail.com>
2020-12-08 14:29:07 +0700

    menambahkan koneksi

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+app.listen(3000,()=>{
+    console.log('Server started on port');
+})
+
+//buat koneksi ke database
+var mysql=require('mysql');
+const conn = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'dbrestapi'
+});
+
+conn.connect((err)=>{
+    if(err) throw err;
+    console.log('mysql terkoneksi');
+});
+
+module.exports=conn;
